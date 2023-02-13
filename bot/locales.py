366fba432535960318321825541b0421054f481a
Uktamjon Komilov <uktamforjob@gmail.com>
2023-02-12 20:48:47 -0800

    bug fixes

--- bot/locales.py
+++ bot/locales.py
@@ -1,12 +1,16 @@
 REQUIRED_ITEMS = "Kerakli narsalar ro'yhati"
 FAQ = "Ko'p so'ralgan savollar"
 CALL_CENTER = """"Call" markaz"""
-LOCATIONS = "Qabul manzillari"
+LOCATIONS = "Qabul manzili"
 
 START = "/start"
 
-WELCOME = "Xush kelibsiz matni"
-INSTRUCTIONS = "Botdan foydalanish matni👇"
+WELCOME = """
+Yangihayot tumanidagi "<a href='https://maps.app.goo.gl/wMDNiqcimRik3aVV8'>TASHKENT INDEX</a>"da turkiyalik qardoshlarimizga gumanitar yordam yig'iladigan markazlashgan punkt tashkil etildi
+
+Turk qardoshlarimizga yordam sifatida 8-fevraldan boshlab yurtimizda gumanitar yordam yig'ila boshlandi. Ana shu harakatlarni markazlashtirish, yordam sifatida taqdim etilayotgan mahsulotlarni saralangan holda qadoqlash, logistikasini yo'lga qo'yish hamda yordam yukini hukumatlararo kelishgan holda manzilli yetkazishni ta’minlash maqsadida 11-fevraldan boshlab, Yangihayot tumanidagi "<a href='https://maps.app.goo.gl/wMDNiqcimRik3aVV8'>TASHKENT INDEX</a>" majmuasida "O'zbekiston xalqidan turk qardoshlarimizga" gumanitar yordam yig'iladigan markazlashgan punkt tashkil etildi.
+"""
+INSTRUCTIONS = "Bo'limni tanlang:👇"
 
 MENU_BUTTONS: list[str] = [
     REQUIRED_ITEMS,
