57e1ad79b302363d9e85176b3772f63359c99499
Uktamjon Komilov <uktamforjob@gmail.com>
2023-02-12 19:31:33 -0800

    str(BASE_DIR/db.sqlite3)

--- core/settings.py
+++ core/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", default="django.db.backends.sqlite3"),
-        "NAME": os.environ.get("DB_NAME", default=BASE_DIR / "db.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", default=str(BASE_DIR/"db.sqlite3")),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
         "USER": os.environ.get("DB_USER"),
